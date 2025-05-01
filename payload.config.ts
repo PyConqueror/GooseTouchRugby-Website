d6de75613169752dec5265ca142d3d28e0902920
PyConqueror <wanaqim@gmail.com>
2025-05-01 15:56:30 +0800

    enable storage adapter for opponent-picture collection

--- payload.config.ts
+++ payload.config.ts
@@ -44,6 +44,7 @@ export default buildConfig({
       collections: {
         media: true,
         'profile-picture': true,
+        'opponent-picture': true,
       },
       token: process.env.BLOB_READ_WRITE_TOKEN,
     }),
